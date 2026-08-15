cafcc39556f4d9f40a0f2e72a714a38ab71ffba1
lazyskiddie <sandeepsingh08550@gmail.com>
2026-08-15 08:11:48 +0530

    make the homeView where all the will merge

---
+++ people connection/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  people connection
+//
+//  Created by singh sandeepkumar vinodkumar on 15/08/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ people connection/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  people connection
+//
+//  Created by singh sandeepkumar vinodkumar on 15/08/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
