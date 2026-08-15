4dec1236ade95dfb37b0c0102b138457d68d6fa7
lazyskiddie <sandeepsingh08550@gmail.com>
2026-08-15 08:17:02 +0530

    the tab view for the all the views

--- people connection/HomeView.swift
+++ people connection/HomeView.swift
@@ -1,15 +1,37 @@
-//
-//  HomeView.swift
-//  people connection
-//
-//  Created by singh sandeepkumar vinodkumar on 15/08/26.
-//
-
 import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            
+            Text("Home View")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            Text("Search View")
+                .tabItem {
+                    Image(systemName: "safari")
+                    Text("Explore")
+                }
+            
+            Text("Notifications View")
+                .tabItem {
+                    Image(systemName: "bell.fill")
+                    Text("Alerts")
+                }
+            Text("Profile View")
+                .tabItem {
+                    Image(systemName: "message.fill")
+                    Text("Chat")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+        }
     }
 }
 
