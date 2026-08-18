6d7e28c1038cbf3a78bb31490c1844320effb5c7
lazyskiddie <sandeepsingh08550@gmail.com>
2026-08-18 09:20:57 +0530

    added the user details regarding the profile

--- people connection/ProfileView.swift
+++ people connection/ProfileView.swift
@@ -1,13 +1,14 @@
-//
-//  ProfileView.swift
-//  people connection
-//
-//  Created by singh sandeepkumar vinodkumar on 15/08/26.
-//
-
 import SwiftUI
 
 struct ProfileView: View {
+    let name = "Sandeep Singh"
+        let age = 22
+        let username = "@Sand0208A"
+        let location = "Surat, Gujarat"
+        let bio = "Software engineer by day, exploring hiking trails by weekend. Love a good cup of coffee and meaningful conversations. Looking for someone to share new experiences with."
+        let interests = ["Travel", "Hiking", "Coding", "Coffee", "Live Music", "Fitness"]
+        
+        let themeColor = Color.purple
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
