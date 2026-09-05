2dca5c27fc0297e421c9cc135b997ee7bf52cb90
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-05 08:47:42 +0530

    chat interface for the users

---
+++ people connection/ChatViews.swift
@@ -0,0 +1,23 @@
+//
+//  ChatViews.swift
+//  people connection
+//
+//  Created by singh sandeepkumar vinodkumar on 05/09/26.
+//
+
+import SwiftUI
+
+struct ChatViews: View {
+    var body: some View {
+        VStack {
+            brandlogo()
+            ScrollView {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ChatViews()
+}
